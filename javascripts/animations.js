1efce63dd3f2efe0fd36ded84f1cce44647d2c52
Manmoth24 <pc.you2480@gmail.com>
2025-12-18 22:00:59 +0900

    スライド作成

--- javascripts/animations.js
+++ javascripts/animations.js
@@ -2,7 +2,7 @@ const crowdedSiteInvite = document.querySelector('nav.crowded-site-invite a');
 
 crowdedSiteInvite.addEventListener('mouseover', () => {
     const crowdedSiteInviteH3 = crowdedSiteInvite.querySelector('h3');
-    crowdedSiteInviteH3.style.fontSize = '2.5em';
+    crowdedSiteInviteH3.style.fontSize = '3em';
 });
 
 crowdedSiteInvite.addEventListener('mouseout', () => {
